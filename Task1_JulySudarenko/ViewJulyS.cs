ba3dd8f6c0eec19d80c37e4940bd8b03542106f3
July Sudarenko <july@sudarenko.ru>
2020-02-18 11:22:33 +0300

    Task1 done

---
+++ Task1_JulySudarenko/ViewJulyS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;//далее можно не писать Console.
+
+namespace Lesson2_CSharp_Lvl1
+{
+    public class ViewJulyS
+    {
+        public static void Print(object value) 
+        {
+            WriteLine(value);        
+        }
+        
+        public static int GetInt()
+        {
+            return Int32.Parse(ReadLine());
+        }
+        
+        public static string GetString()
+        {
+            return ReadLine();
+        }
+        
+        public static void Pause()
+        {
+            ReadKey();
+        }
+    }
+}
